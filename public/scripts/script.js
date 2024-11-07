834d6cc475d51c91bdf44ebc911cb16304204684
yasinthamadusanka <yasinthamadusanka.a@gmail.com>
2024-11-07 19:30:11 +0530

    Done the password toggle

---
+++ public/scripts/script.js
@@ -0,0 +1,14 @@
+// Function to toggle the password visibility
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const eyeIcon = document.getElementById('eye-icon');
+
+    // Toggle the password type between 'password' and 'text'
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.name = "eye-outline"; // Change icon to 'eye' when visible
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.name = "eye-off-outline"; // Change icon to 'eye-off' when hidden
+    }
+}
