bf9d25c085412f63ec570183f776b510e31e2322
yasinthamadusanka <yasinthamadusanka.a@gmail.com>
2024-11-08 10:13:40 +0530

    Done the dotenv

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'user1',
-    password: 'password',      
-    database: 'fitness_coach'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
