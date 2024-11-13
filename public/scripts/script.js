94385ce667e71dfcd85eb7ec3edcde5165d1d864
yasinthamadusanka <yasinthamadusanka.a@gmail.com>
2024-11-13 13:23:35 +0530

    Fixed the fotgot password

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -12,3 +12,17 @@ function togglePasswordVisibility() {
         eyeIcon.name = "eye-off-outline"; // Change icon to 'eye-off' when hidden
     }
 }
+
+function togglePasswordVisibility2(){
+    const passwordInput = document.getElementById('newPassword');
+    const eyeIcon = document.getElementById('eye-icon');
+
+    // Toggle the password type between 'password' and 'text'
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.name = "eye-outline"; // Change icon to 'eye' when visible
+    } else {
+        passwordInput.type = "password";
+        eyeIcon.name = "eye-off-outline"; // Change icon to 'eye-off' when hidden
+    }
+}
